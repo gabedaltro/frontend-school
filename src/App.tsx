612a74b5c842139887a9683f79abb200f397baaa
Gabriel Guimarães <gabeguimaraes13@gmail.com>
2022-12-03 18:09:58 -0300

    Ajusta App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
+import { useEffect, useState } from "react";
+import { theme } from "./config/theme";
+import AppRoutes from "./routes/Routes";
+import { AppProvider } from "./hooks/app";
 import { ThemeProvider } from "@mui/material";
-import { theme } from "config/theme";
-import { AppProvider } from "hooks/app";
-import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { mobileCheck } from "./helpers/MobileCheck";
 
-function App() {
-  const [isOpenedMenu, setIsOpenedMenu] = useState(false);
+const App: React.FC = () => {
+  const [isMobile, setIsMobile] = useState(mobileCheck());
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isOpenedMenu, setIsOpenedMenu] = useState(
+    !mobileCheck() && windowWidth > 1280
+  );
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  function handleResize() {
+    setIsMobile(mobileCheck());
+    setWindowWidth(window.innerWidth);
+  }
 
   function handleOpenMenu() {
     setIsOpenedMenu(!isOpenedMenu);
   }
 
   return (
-    <AppProvider value={{ handleOpenMenu, isOpenedMenu }}>
-      <ThemeProvider theme={theme}></ThemeProvider>
-    </AppProvider>
+    <BrowserRouter>
+      <AppProvider
+        value={{ handleOpenMenu, isOpenedMenu, isMobile, windowWidth }}
+      >
+        <ThemeProvider theme={theme}>
+          <AppRoutes />
+        </ThemeProvider>
+      </AppProvider>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
 
