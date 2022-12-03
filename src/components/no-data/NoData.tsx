de6ee0c2c4976249901d98c5525a70fa19d7a7ba
Gabriel Guimarães <gabeguimaraes13@gmail.com>
2022-12-03 18:30:11 -0300

    Adiciona NoData

---
+++ src/components/no-data/NoData.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+    flex: 1,
+    background: '#fff',
+    marginTop: 10,
+  },
+});
+
+type NoDataProps = {
+  message: string;
+};
+
+const NoData: React.FC<NoDataProps> = ({ message }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <Typography>{message}</Typography>
+    </div>
+  );
+};
+
+export default NoData;
