d6c1e2faa7e3321af523e3ec2bba719513517531
Gabriel Guimarães <gabeguimaraes13@gmail.com>
2022-12-05 23:17:54 -0300

    Adiciona novas rotas

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,7 +2,12 @@ import RouteApp from "./Route";
 import Home from "../pages/home/Home";
 import { Route, Routes } from "react-router-dom";
 import StudentPage from "../pages/student/Student";
+import TeacherPage from "../pages/teacher/Teacher";
 import Error404 from "../components/error/Error404";
+import Classroom from "../pages/classroom/Clasroom";
+import ReportCard from "../pages/report-card/ReportCard";
+import StudentNew from "../pages/student/registration/new/StudentNew";
+import TeacherNew from "../pages/teacher/registration/new/TeacherNew";
 
 const AppRoutes = () => {
   return (
@@ -10,12 +15,38 @@ const AppRoutes = () => {
       <Route path="*" element={<RouteApp element={<Error404 />} />} />
 
       <Route path="/" element={<RouteApp element={<Home />} />} />
-      <Route path="/class" element={<RouteApp element={<Home />} />} />
-      <Route path="/report-card" element={<RouteApp element={<Home />} />} />
-      <Route path="/teachers" element={<RouteApp element={<Home />} />} />
+
+      <Route path="/classroom" element={<RouteApp element={<Classroom />} />} />
+      <Route
+        path="/classroom/new"
+        element={<RouteApp element={<Classroom />} />}
+      />
+
+      <Route
+        path="/report-card"
+        element={<RouteApp element={<ReportCard />} />}
+      />
+      <Route
+        path="/report-card/new"
+        element={<RouteApp element={<ReportCard />} />}
+      />
+
+      <Route
+        path="/teachers"
+        element={<RouteApp element={<TeacherPage />} />}
+      />
+      <Route
+        path="/teacher/new"
+        element={<RouteApp element={<TeacherNew />} />}
+      />
+
       <Route
         path="/students"
         element={<RouteApp element={<StudentPage />} />}
+      />
+      <Route
+        path="/student/new"
+        element={<RouteApp element={<StudentNew />} />}
       />
     </Routes>
   );
