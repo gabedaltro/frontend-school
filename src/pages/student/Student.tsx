0d2f5c8821e5b125be4e8f0f420118ae24a2061e
Gabriel Guimarães <gabeguimaraes13@gmail.com>
2022-12-07 13:37:37 -0300

    Ajusta Student

--- src/pages/student/Student.tsx
+++ src/pages/student/Student.tsx
@@ -5,10 +5,17 @@ import { useApp } from "../../hooks/app";
 import { api } from "../../services/api";
 import { Student } from "../../types/student";
 import { useNavigate } from "react-router-dom";
+import useTableOrder from "../../hooks/tableOrder";
 import Appbar from "../../components/appbar/Appbar";
 import NoData from "../../components/no-data/NoData";
+import PaginationProvider from "../../hooks/pagination";
 import { useMessaging } from "../../providers/messaging";
+import StudentListTable from "./list/table/StudentListTable";
+import { studentTableTemplate } from "./studentTableTemplate";
+import StudentListModule from "./list/module/StudentListModule";
+import Pagination from "../../components/pagination/Pagination";
 import TableLoading from "../../components/loading/TableLoading";
+import TableContainer from "../../components/table/TableContainer";
 import ModuleLoading from "../../components/loading/ModuleLoading";
 import PageHeaderActions from "../../components/page-header/PageHeaderActions";
 
@@ -26,8 +33,10 @@ const StudentPage: React.FC = () => {
   const classes = useStyles();
   const history = useNavigate();
   const messaging = useMessaging();
+  const [orderedIndex, sort] = useTableOrder();
   const [loading, setLoading] = useState(false);
   const [students, setStudents] = useState<Student[]>([]);
+  const [filtered, setFiltered] = useState<Student[]>([]);
   const [displayMode, setDisplayMode] = useState<"list" | "module">("list");
 
   useEffect(() => {
@@ -35,6 +44,11 @@ const StudentPage: React.FC = () => {
   }, [app.isMobile, app.windowWidth]);
 
   useEffect(() => {
+    setFiltered(students);
+  }, [students]);
+
+  useEffect(() => {
+    setLoading(true);
     api
       .get("/student")
       .then((response) => setStudents(response.data))
@@ -46,8 +60,13 @@ const StudentPage: React.FC = () => {
       });
   }, []);
 
+  function handleSort(index: string) {
+    const p = sort(index, filtered);
+    setFiltered(p);
+  }
+
   return (
-    <div>
+    <>
       <Appbar title="Alunos" />
       <PageHeaderActions
         title="Alunos"
@@ -66,7 +85,7 @@ const StudentPage: React.FC = () => {
         }
       />
 
-      <div className={classes.container}>
+      <TableContainer tableTemplate={studentTableTemplate}>
         {loading ? (
           displayMode === "list" ? (
             <TableLoading />
@@ -76,10 +95,25 @@ const StudentPage: React.FC = () => {
         ) : students.length === 0 ? (
           <NoData message="Nenhum aluno matriculado." />
         ) : (
-          <>existe</>
+          <PaginationProvider>
+            <div className={classes.container}>
+              {displayMode === "list" ? (
+                <StudentListTable
+                  students={filtered}
+                  handleSort={handleSort}
+                  orderedIndex={orderedIndex}
+                />
+              ) : (
+                displayMode === "module" && (
+                  <StudentListModule students={filtered} />
+                )
+              )}
+              <Pagination count={filtered.length} />
+            </div>
+          </PaginationProvider>
         )}
-      </div>
-    </div>
+      </TableContainer>
+    </>
   );
 };
 
