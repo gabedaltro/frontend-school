4d41cf22b44cba8864bd72db5fdcdfc1e1c1ba65
Gabriel Guimarães <gabeguimaraes13@gmail.com>
2022-12-07 13:36:50 -0300

    Remove overflow de TableContent

--- src/components/table/TableContent.tsx
+++ src/components/table/TableContent.tsx
@@ -1,14 +1,13 @@
-import React, { ReactNode } from 'react';
-import { makeStyles } from '@mui/styles';
+import React, { ReactNode } from "react";
+import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles({
   container: {
-    backgroundColor: '#fff',
-    margin: '10px 0',
-    width: '100%',
-    minHeight: 'calc(100vh - 330px)',
-    overflow: 'auto',
-    position: 'relative',
+    backgroundColor: "#fff",
+    margin: "10px 0",
+    width: "100%",
+    minHeight: "calc(100vh - 330px)",
+    position: "relative",
   },
 });
 
